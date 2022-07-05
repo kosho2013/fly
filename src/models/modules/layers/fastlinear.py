185f5b0e9618ddb8a08c8b92151136f3967dbfaa
Sho Ko <kosho2013@hotmail.com>
2022-07-05 01:50:02 -0700

    new

--- src/models/modules/layers/fastlinear.py
+++ src/models/modules/layers/fastlinear.py
@@ -739,7 +739,7 @@ class SparseLRLinear(nn.Module):
         return self.sparse.saving + self.low_rank.saving
 
     def _multiply(self, x):
-        print('\n\n\n\n\n\n\n\n\n\n\n\n\n', type(x), x.dtype, x.get_device(), '\n\n\n\n\n\n\n\n\n\n\n\n\n')
+        print('\n\n\n\n\n\n\n\n\n\n\n\n\n', 'x', type(x), x.dtype, x.get_device(), '\n\n\n\n\n\n\n\n\n\n\n\n\n')
         
         
         
@@ -747,6 +747,12 @@ class SparseLRLinear(nn.Module):
         low_rank_output = self.low_rank(x)
         g = torch.sigmoid(self.gate(x)) if self.gate is not None else 0.5
         # output = (1.0 - g) * sparse_output + g * low_rank_output
+        
+        
+        
+        print('\n\n\n\n\n\n\n\n\n\n\n\n\n', 'sparse_output', type(sparse_output), sparse_output.dtype, sparse_output.get_device(), '\n\n\n\n\n\n\n\n\n\n\n\n\n')
+        print('\n\n\n\n\n\n\n\n\n\n\n\n\n', 'low_rank_output', type(low_rank_output), low_rank_output.dtype, low_rank_output.get_device(), '\n\n\n\n\n\n\n\n\n\n\n\n\n')
+        
         return torch.lerp(sparse_output, low_rank_output, g)
 
     def forward(self, x):
